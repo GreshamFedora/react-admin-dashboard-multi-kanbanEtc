8fa1281e1dd58c66b4bdbd8dbd0928bf2df7e244
Gresham Fedora <gresham.fedora@gmail.com>
2023-01-22 12:20:11 -0500

    set up, settings button added, sidebar placeholder added

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+// plugins
+module.exports = {
+    style: {
+      postcss: {
+        plugins: [
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ],
+      },
+    },
+  };
