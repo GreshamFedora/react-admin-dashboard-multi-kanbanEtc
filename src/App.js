69df372ddbad7b56d92e6cd55c6838d822dcd8cd
Gresham Fedora <gresham.fedora@gmail.com>
2023-01-22 19:42:16 -0500

    sidebar and divs - navbar

--- src/App.js
+++ src/App.js
@@ -24,10 +24,20 @@ const App = () => {
             Sidebar
           </div>
         ):(
-          <div>
-            Sidebar w-0  
+          <div className='w-0 dark:bg-secondary-dark-bg'>
+            Sidebar  
           </div>
         )}
+        <div className={
+          `dark:bg-main-bg bg-main-bg min-h-screen w-full
+          ${activeMenu ? 'md:ml-71' 
+                     : 'flex-2'}`
+        }>
+          <div classname="fixed md:static bg-main-bg dark:bg-main-bd navbar w-full">
+
+          </div>
+          
+        </div>
       </div>
     </BrowserRouter>
   )
