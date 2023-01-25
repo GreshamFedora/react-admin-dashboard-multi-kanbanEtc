b1da082e06a4efbbf713fe15da2695ae7e1b9a9b
Gresham Fedora <gresham.fedora@gmail.com>
2023-01-25 08:42:07 -0500

    setting up Orders component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='mb-10'>
+      <p className='text-gray-400'>
+        {category}
+      </p>
+      <p className='text-3xl font-extabold tracking-tight text-slate-900'>
+        {title}
+      </p>
+    </div>
   )
 }
 
